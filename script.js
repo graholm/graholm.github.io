184cc6ad076f0541ff2e7c747b96e6c56cb81c04
graholm <graholm@nuevaschool.org>
2016-11-30 22:03:14 -0800

    Classadoo autocommit

---
+++ script.js
@@ -0,0 +1,10 @@
+function logArgs(args) {
+  var args = 1
+  console.log(args)
+}
+
+var functionString = logArgs.toString()
+var consoleLogExample = $("#console-log-example")
+var formattedExample = $("<pre>")
+formattedExample.text(functionString)
+consoleLogExample.html(formattedExample)
